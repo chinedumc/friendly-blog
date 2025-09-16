5f093721feca22a528084863a9855e08cd89bcf9
chinedumc <chibokachinedum@gmail.com>
2025-09-16 23:04:22 +0100

    React-Router framework mode: initial setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "The friendly blog" },
+    { name: "description", content: "Custom Friendly Blog" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>My App</>
 }
