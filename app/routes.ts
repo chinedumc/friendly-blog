a5200e17896e78b25ca8e233efdcfee946c519f2
chinedumc <chibokachinedum@gmail.com>
2025-09-19 23:22:21 +0100

    added more routes

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
 	index("routes/home/index.tsx"),
 	route("about", "./routes/about/index.tsx"),
+	route("contact", "./routes/contact/index.tsx"),
+	route("projects", "./routes/projects/index.tsx"),
 ] satisfies RouteConfig;
