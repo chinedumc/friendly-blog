7b0762e45c1b2959bda08267f639932ba944434d
chinedumc <chibokachinedum@gmail.com>
2025-09-22 18:29:45 +0100

    added a hero component and layout routes

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,17 +1,21 @@
 // import type { Route } from "./+types/home";
-import { log } from "console";
+// import Hero from "~/components/Hero";
 import type { Route } from "./+types";
 
-
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Welcome" },
-    { name: "description", content: "Custom Friendly Blog" },
-  ];
+	return [
+		{ title: "Welcome" },
+		{ name: "description", content: "Custom Friendly Blog" },
+	];
 }
 
 export default function Home() {
-  // console.log("hello...");
-  
-  return <>My App</>
+	// console.log("hello...");
+
+	return (
+		<>
+			{/* <Hero name="Nedum" /> */}
+      Home page
+		</>
+	);
 }
