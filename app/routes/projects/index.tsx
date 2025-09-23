43278c6f10aa94b4ce83b69c1bfb3536759e0c85
chinedumc <chibokachinedum@gmail.com>
2025-09-23 23:08:24 +0100

    installed json-server for db

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,3 +1,12 @@
+import type { Route } from "./+types";
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+	const res = await fetch("http://localhost:8000/projects");
+	const data = await res.json();
+
+	return { projects: data };
+}
+
 const ProjectsPage = () => {
 	return <>Projects Page</>;
 };
