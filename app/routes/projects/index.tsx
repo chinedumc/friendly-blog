d10bb0ac8f6c03cbab551836e29b891b6ad3ae9c
chinedumc <chibokachinedum@gmail.com>
2025-09-24 21:30:35 +0100

    Fetch projects with Loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,13 +1,23 @@
 import type { Route } from "./+types";
+import type { Project } from "~/types";
 
-export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+export async function loader({
+	request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
 	const res = await fetch("http://localhost:8000/projects");
 	const data = await res.json();
 
 	return { projects: data };
 }
 
-const ProjectsPage = () => {
-	return <>Projects Page</>;
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+	const { projects } = loaderData as { projects: Project[] };
+	console.log(projects);
+
+	return (
+		<>
+			<h2 className="text-3xl font-bold mb-8"> Projects</h2>
+		</>
+	);
 };
 export default ProjectsPage;
