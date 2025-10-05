7f5429eb9fec3eb5054bd615bc47899f9c936716
chinedumc <chibokachinedum@gmail.com>
2025-10-05 22:48:06 +0100

    Contact Page setup

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,5 +1,9 @@
 const ContactPage = () => {
-	return <>Contact Page</>;
+	return (
+		<div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
+			<h2 className="text-3xl font-bold text-white mb-8 text-center">Contact Page</h2>
+		</div>
+	);
 };
 
 export default ContactPage;
