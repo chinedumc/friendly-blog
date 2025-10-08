adab8b5d2219923b8a7962449781e2677fd7eb45
chinedumc <chibokachinedum@gmail.com>
2025-10-08 23:07:09 +0100

    Form validation

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -3,10 +3,25 @@ import { Form } from "react-router";
 
 export async function action({ request }: Route.ActionArgs) {
 	const formData = await request.formData();
-	const name = formData.get("name");
-	const email = formData.get("email");
-	const subject = formData.get("subject");
-	const message = formData.get("message");
+	const name = formData.get("name") as string;
+	const email = formData.get("email") as string;
+	const subject = formData.get("subject") as string;
+	const message = formData.get("message") as string;
+
+	const errors: Record<string, string> = {};
+
+	if (!name) errors.name = "Name is required";
+	if (!email) {
+		errors.email = "email is required";
+	} else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+		errors.email = "Invalid Email format";
+	}
+	if (!subject) errors.subject = "Subject is required";
+	if (!message) errors.message = "Message is required";
+
+	if (Object.keys(errors).length > 0) {
+		return { errors };
+	}
 
 	const data = { name, email, subject, message };
 
@@ -14,6 +29,7 @@ export async function action({ request }: Route.ActionArgs) {
 }
 
 const ContactPage = ({ actionData }: Route.ComponentProps) => {
+	const errors = actionData?.errors || {};
 	return (
 		<div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
 			<h2 className="text-3xl font-bold text-white mb-8 text-center">
@@ -42,6 +58,9 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
 						name="name"
 						className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
 					/>
+					{errors.name && (
+						<p className="text-red-400 text-sm mt-1">{errors.name}</p>
+					)}
 				</div>
 				<div>
 					<label
@@ -58,6 +77,9 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
 						name="email"
 						className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
 					/>
+					{errors.email && (
+						<p className="text-red-400 text-sm mt-1">{errors.email}</p>
+					)}
 				</div>
 				<div>
 					<label
@@ -74,6 +96,9 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
 						name="subject"
 						className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
 					/>
+					{errors.subject && (
+						<p className="text-red-400 text-sm mt-1">{errors.subject}</p>
+					)}
 				</div>
 				<div>
 					<label
@@ -89,6 +114,9 @@ const ContactPage = ({ actionData }: Route.ComponentProps) => {
 						name="message"
 						className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
 					/>
+					{errors.message && (
+						<p className="text-red-400 text-sm mt-1">{errors.message}</p>
+					)}
 				</div>
 
 				<button className="w-full bg-blue-600 text-white py-2 rounded-lg  hover:bg-blue-700 cursor-pointer">
