a1786b1b9228c198a49e938fbfe979839e6b9b85
chinedumc <chibokachinedum@gmail.com>
2025-10-11 00:43:26 +0100

    Fetch and Display Post Meta

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,7 +1,6 @@
-import { log } from "console";
 import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
-import { Link } from "react-router";
+import PostCard from "../../components/PostCard";
 
 export async function loader({
 	request,
@@ -24,14 +23,7 @@ const BlogsPage = ({ loaderData }: Route.ComponentProps) => {
 		<div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
 			<h3 className="text-3xl text-white font-bold mb-8">Blog Page</h3>
 			{posts.map((post) => (
-				<article className="bg-gray-800 p-6 rounded-lg shadow mb-4">
-					<h3 className="text-2xl font-semibold text-blue-400">{post.title}</h3>
-					<p className="text-sm text-gray-400 mb-2">
-						{new Date(post.date).toDateString()}
-					</p>
-					<p className="text-gray-300 mb-4">{post.excerpt}</p>
-					<Link to={`/blog/${post.slug}`} className="text-blue-300 text-sm hover:underline">Read More...</Link>
-				</article>
+				<PostCard key={post.slug} post={post} />
 			))}
 		</div>
 	);
