beebad48f1d5c26afbfcdd859d46bd081f00279a
chinedumc <chibokachinedum@gmail.com>
2025-10-22 00:46:40 +0100

    Setup Strapi Project type

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import ProjectCard from "~/components/ProjectCard";
 import type { Route } from "./+types";
-import type { Project } from "~/types";
+import type { Project, StrapiProject, StrapiResponse } from "~/types";
 import Pagination from "~/components/Pagination";
 import { AnimatePresence, motion } from "framer-motion";
 
@@ -15,10 +15,26 @@ export function meta({}: Route.MetaArgs) {
 export async function loader({
 	request,
 }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-	const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-	const data = await res.json();
+	const res = await fetch(
+		`${import.meta.env.VITE_API_URL}/projects?populate=*`
+	);
+	const json:StrapiResponse<StrapiProject> = await res.json();
 
-	return { projects: data };
+	const projects = json.data.map((item) => ({
+		id: item.id,
+		documentId: item.documentId,
+		title: item.title,
+		description: item.description,
+		image: item.image?.url
+			? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}`
+			: "/images/no-image.png",
+		url: item.url,
+		date: item.date,
+		category: item.category,
+		featured: item.featured,
+	}));
+
+	return {  projects };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
